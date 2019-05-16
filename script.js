b0cb61960cb992e38b0f7c763078ddbc3479dee6
Firas Abuzaid <fabuzaid21@gmail.com>
2019-05-15 17:20:22 -0700

    added search box

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 "use strict";
 
 // TODO:
-// 1) Add Search bar, so user can type in query
-// 2) Add list interface:
+// 1) Add list interface:
 //    * create list
 //    * share list
 //    * delete list
 //    * list members
+// 2) Marker should update location as you click on map
 // 3) Click on place and add to list
 // 4) Share list logic
 // 5) Google Account support
@@ -14,8 +14,8 @@
 // 7) Add autocomplete for search
 
 var map;
-var service;
 var infowindow;
+
 
 function createMarker(place) {
   var marker = new google.maps.Marker({
@@ -27,31 +27,72 @@ function createMarker(place) {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
   });
+  return marker;
 }
 
+
 function initMap() {
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
 
   infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(
-    document.getElementById('map'), {center: sydney, zoom: 15});
+    document.getElementById('map'), {
+     center: {lat: 37.4275, lng: -122.1697},
+     zoom: 15
+    });
 
-  var request = {
-    query: 'Museum of Contemporary Art Australia',
-    fields: ['name', 'geometry'],
-  };
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  service = new google.maps.places.PlacesService(map);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
 
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
 
-      map.setCenter(results[0].geometry.location);
+    if (places.length == 0) {
+      return;
     }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(createMarker(place));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 }
 
